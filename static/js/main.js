c5fdb3fc20581e0b893bea13a80770613c5c672f
Dillon Estrada <dillon.estrada55@gmail.com>
2020-06-26 16:27:50 -0500

    change layout and background and add gsap.js with plugins

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(MotionPathPlugin, TextPlugin, ScrollToPlugin, ScrollTrigger);
+
+
+gsap.fromTo(".title", {opacity: 0}, {duration:5, opacity:1.0});
+
+function hover(side) {
+	sideElement = document.getElementById(side);
+	console.log(side);
+	console.log(sideElement);
+	sideElement.addEventListener("mouseover", function(){
+			gsap.to("#" + side, {duration: .5, border: "solid white"});
+	});
+	sideElement.addEventListener("mouseout", function(){
+			gsap.to("#" + side, {duration: .5, border: "none"});
+	});
+};
+
+hover("left-side");
