73a3cbcceeb8f9c1f2a5a2ae529693532608713e
Dillon Estrada <dillon.estrada55@gmail.com>
2020-06-25 19:58:48 -0500

    add base.html template and set-up rough draft index page

--- site.py
+++ site.py
@@ -3,7 +3,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index.html', title="Home")
+
+# future routes
+"""
+@app.route('/programmer')
+def programmer():
+	return render_template('')
+
+@app.route('/musician')
+def musician():
+	return render_template('')
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
