bee5b7daec7bc260bc310a32a3aa4f19150e0684
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-12 05:08:02 -0500

    add some responsiveness

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // Initialize MaterializeCSS components
   $('.scrollspy').scrollSpy({scrollOffset: 200, activeClass: 'scrollspy-active'});
-  
+  $('.fixed-action-btn').floatingActionButton({hoverEnabled: false});
   // Event listeners
 
 });
