780fcdf4bfee368bf408b9ae21388cdb3d7a087f
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-15 15:36:15 -0500

    add dark mode button and functionality

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
   // Initialize MaterializeCSS components
   $('.scrollspy').scrollSpy({scrollOffset: 200, activeClass: 'scrollspy-active'});
   $('.fixed-action-btn').floatingActionButton({hoverEnabled: false});
+
+  $(".toggle > input").on('click', () => {
+    let mode = $('body').attr('data-theme');
+    let modeAlternates = {
+      dark: "light",
+      light: "dark"
+    }
+    $('body').attr('data-theme', modeAlternates[mode]);
+    let modeLabelText = modeAlternates[mode].charAt(0).toUpperCase() + modeAlternates[mode].slice(1)
+    $('#mode').text(modeLabelText);
+    // if($('body').attr('data-theme') == "dark"){
+    //   $('body').attr('data-theme', 'light');
+
+    // } else {
+    //   $('body').attr('data-theme', 'dark');
+    // }
+  })
 });
