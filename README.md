c3f8753bbf1a77d95dccccd82b2c3ac462547a68
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-16 18:29:57 -0500

    rework layout and projects sections

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Personal Website
+# Dillon's Personal Website
+This is my personal web developer portfolio website designed to hold my projects and be an online resume for potential clients.
+
+## Design
+This was my first dive into using and understanding css animations. Previously, I had used all Javascript based animation, but with this site I wanted to make performance a priority and learn some new things as well.
+
+I used `materialize.css` for the form components and a few buttons. In later commits I will be limiting the materialize code in the codebase to only the components that I'm utilizing.
+
+## THIS README IS A WORK IN PROGRESS
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // Initialize MaterializeCSS components
-  $('.scrollspy').scrollSpy({scrollOffset: 50, activeClass: 'scrollspy-active'});
-  $('.fixed-action-btn').floatingActionButton({hoverEnabled: false});
+  $('.scrollspy').scrollSpy({
+    scrollOffset: 0,
+    activeClass: 'scrollspy-active'
+  });
+  $('.fixed-action-btn').floatingActionButton({
+    hoverEnabled: false
+  });
 
   $(".toggle > input").on('click', () => {
     let mode = $('body').attr('data-theme');
@@ -13,11 +18,20 @@ $(document).ready(function(){
     $('body').attr('data-theme', modeAlternates[mode]);
     let modeLabelText = modeAlternates[mode].charAt(0).toUpperCase() + modeAlternates[mode].slice(1)
     $('.mode').text(modeLabelText);
-    // if($('body').attr('data-theme') == "dark"){
-    //   $('body').attr('data-theme', 'light');
+  })
 
-    // } else {
-    //   $('body').attr('data-theme', 'dark');
-    // }
+  let sections = document.querySelectorAll('section')
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting == true) {
+        entry.target.classList.add('fade-in')
+      } else {
+        entry.target.classList.remove('fade-in')
+      }
+    })
+  })
+
+  sections.forEach(section => {
+    observer.observe(section)
   })
 });
