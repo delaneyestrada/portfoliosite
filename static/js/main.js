3bf3f3228695a891a89017bf4d592ebb21389bf0
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-20 13:42:17 -0500

    Responsiveness update and sass file structure update

--- static/js/main.js
+++ static/js/main.js
@@ -14,24 +14,24 @@ $(document).ready(function () {
       dark: "light",
       light: "dark"
     }
-    $('.toggle-container i').toggleClass('rotate');
+    // $('.toggle-container i').toggleClass('rotate');
     $('body').attr('data-theme', modeAlternates[mode]);
     let modeLabelText = modeAlternates[mode].charAt(0).toUpperCase() + modeAlternates[mode].slice(1)
     $('.mode').text(modeLabelText);
   })
 
-  let sections = document.querySelectorAll('section')
-  let observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting == true) {
-        entry.target.classList.add('fade-in')
-      } else {
-        entry.target.classList.remove('fade-in')
-      }
-    })
-  })
+  // let sections = document.querySelectorAll('section')
+  // let observer = new IntersectionObserver((entries) => {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting == true) {
+  //       entry.target.classList.add('fade-in')
+  //     } else {
+  //       entry.target.classList.remove('fade-in')
+  //     }
+  //   })
+  // })
 
-  sections.forEach(section => {
-    observer.observe(section)
-  })
+  // sections.forEach(section => {
+  //   observer.observe(section)
+  // })
 });
