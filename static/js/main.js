5eb4822e107189e7055a02099d478a91931add8a
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-24 15:01:08 -0500

    use local storage to keep dark mode setting on new page load

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,42 @@
 $(document).ready(function () {
-  // Initialize MaterializeCSS components
-  $('.scrollspy').scrollSpy({
-    scrollOffset: 0,
-    activeClass: 'scrollspy-active'
-  });
-  $('.fixed-action-btn').floatingActionButton({
-    hoverEnabled: false
-  });
+    // Initialize MaterializeCSS components
+    $(".scrollspy").scrollSpy({
+        scrollOffset: 0,
+        activeClass: "scrollspy-active",
+    });
 
-  $(".toggle > input").on('click', () => {
-    let mode = $('body').attr('data-theme');
-    let modeAlternates = {
-      dark: "light",
-      light: "dark"
+    // DARK MODE FUNCTIONALITY
+    let mode = window.localStorage.getItem("data-theme");
+    if (!mode) {
+        window.localStorage.setItem("data-theme", $("body").attr("data-theme"));
     }
-    // $('.toggle-container i').toggleClass('rotate');
-    $('body').attr('data-theme', modeAlternates[mode]);
-    let modeLabelText = modeAlternates[mode].charAt(0).toUpperCase() + modeAlternates[mode].slice(1)
-    $('.mode').text(modeLabelText);
-  })
+    $("body").attr("data-theme", mode);
 
-  // let sections = document.querySelectorAll('section')
-  // let observer = new IntersectionObserver((entries) => {
-  //   entries.forEach((entry) => {
-  //     if (entry.isIntersecting == true) {
-  //       entry.target.classList.add('fade-in')
-  //     } else {
-  //       entry.target.classList.remove('fade-in')
-  //     }
-  //   })
-  // })
+    $(".toggle > input").on("click", () => {
+        let modeAlternates = {
+            dark: "light",
+            light: "dark",
+        };
+        $("body").attr("data-theme", modeAlternates[mode]);
+        let modeLabelText =
+            modeAlternates[mode].charAt(0).toUpperCase() +
+            modeAlternates[mode].slice(1);
+        $(".mode").text(modeLabelText);
+        window.localStorage.setItem("data-theme", modeAlternates[mode]);
+    });
 
-  // sections.forEach(section => {
-  //   observer.observe(section)
-  // })
+    // let sections = document.querySelectorAll('section')
+    // let observer = new IntersectionObserver((entries) => {
+    //   entries.forEach((entry) => {
+    //     if (entry.isIntersecting == true) {
+    //       entry.target.classList.add('fade-in')
+    //     } else {
+    //       entry.target.classList.remove('fade-in')
+    //     }
+    //   })
+    // })
+
+    // sections.forEach(section => {
+    //   observer.observe(section)
+    // })
 });
